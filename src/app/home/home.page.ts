6d5e2b5e7bcffe17980260fbac533a52b130e1a9
Voraton Nuttakarnnuwat <riskmaster2468@gmail.com>
2019-07-15 14:17:08 +0700

    นำค่าที่ได้จากการคำนวณ ออกมาแสดงที่หน้า

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,14 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
+  result;
+
   constructor(private http:HttpClient) {
-    // this.onClickCal();
+    //this.onClickCal();
   }
 
   async onClickCal(Set,Plus) {
     let url = "https://nextflow-node-calculator-api.azurewebsites.net/calculator/plus";
     
     let response = await<any> this.http.post(url,{ "first" : parseInt(Set), "second": parseInt(Plus) }).toPromise();
+
+    this.result = response.result;
 
     console.log('Set',Set,'Plus',Plus,'response',response);
   }

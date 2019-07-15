c2b284f141e7a1ae2f9d7e12b21d9661c7646816
Voraton Nuttakarnnuwat <riskmaster2468@gmail.com>
2019-07-15 13:43:32 +0700

    เตรียมส่ง code requies ไปที่ api เพื่อรอคำนวณ

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,10 +12,12 @@ export class HomePage {
     // this.onClickCal();
   }
 
-  onClickCal(Set,Plus) {
-    let url = "https://nextflow-node-calculator-api.azurewebsites.net/calculator/plus"
-    let respones = await<any> this.http.post(https://nextflow-node-calculator-api.azurewebsites.net/calculator/plus)
-    console.log('Set',Set,'Plus',Plus);
+  async onClickCal(Set,Plus) {
+    let url = "https://nextflow-node-calculator-api.azurewebsites.net/calculator/plus";
+    
+    let response = await<any> this.http.post(url,{}).toPromise();
+
+    // console.log('Set',Set,'Plus',Plus);
   }
 
 }
